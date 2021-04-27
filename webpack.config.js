6b2fd6fae74b7f2dd63ae74482f8c2f2c9cfaa2d
Sanghak <k3hppk@gmail.com>
2021-04-27 21:28:09 +0900

    Add inline source map to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
