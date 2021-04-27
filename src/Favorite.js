04adc106cc023bc4243c210722fdae86d0318023
Sanghak <k3hppk@gmail.com>
2021-04-27 21:19:59 +0900

    Issue2: Create Favorites component that handles localStorage
    This commit creates a basic Favorites file that can add and remove users
    from localStorage.

---
+++ src/Favorite.js
@@ -0,0 +1,36 @@
+import searchDummyData from './searchDummyData';
+
+function LocalStorage() {
+  const getSearchDummyData = () => {
+    return searchDummyData;
+  };
+
+  const storeUserData = (userData) => {
+    localStorage.setItem('users', JSON.stringify(userData));
+  };
+
+  const getUserData = () => {
+    return JSON.parse(localStorage.getItem('users'));
+  };
+
+  const removeUser = (userName) => {
+    const newUserData = getUserData().filter((user) => user.login !== userName);
+    storeUserData(newUserData);
+  };
+
+  const addUser = (userData) => {
+    const newUserData = [...getUserData(), userData];
+    storeUserData(newUserData);
+  };
+
+  return {
+    getSearchDummyData,
+    removeUser,
+    addUser,
+    getUserData,
+  };
+}
+
+const Favorites = LocalStorage();
+
+export default Favorites;
