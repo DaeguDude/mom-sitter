a9320bfaaec66dc0504626211d6e3db33e7ccbf3
Sanghak <k3hppk@gmail.com>
2021-04-27 12:33:10 +0900

    Update webpack config to use topLevel await

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  experiments: {
+    topLevelAwait: true,
   },
 };
