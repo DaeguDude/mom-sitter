3c5eeafbc0af9bc37a7f8080945c61ac4785ac47
Sanghak <k3hppk@gmail.com>
2021-04-29 14:56:00 +0900

    Remove get detailed user info feature

--- src/getUser.js
+++ src/getUser.js
@@ -8,18 +8,10 @@ const octokit = new Octokit({
 async function getUser(name) {
   const searchResponse = await octokit.request('GET /search/users', {
     q: name,
-    headers: {
-      accept: 'application/vnd.github.v3.text-match+json',
-    },
-  });
-
-  const getResponse = await octokit.request('GET /users/{username}', {
-    username: name,
   });
 
   console.log('Search Response: ', searchResponse);
   console.log('-----');
-  console.log('Get Response: ', getResponse);
 }
 
 export default getUser;
