7dd2907f6a157d5a322e8d058668f5c0c3f6b547
Sanghak <k3hppk@gmail.com>
2021-04-29 11:01:38 +0900

    Add feature that gets more detail userInfo

--- src/getUser.js
+++ src/getUser.js
@@ -5,13 +5,21 @@ const octokit = new Octokit({
   auth: config.githubToken,
 });
 
-async function getUser(usersToSearch) {
-  console.log(usersToSearch);
-  const response = await octokit.request('GET /search/users', {
-    q: usersToSearch,
+async function getUser(name) {
+  const searchResponse = await octokit.request('GET /search/users', {
+    q: name,
+    headers: {
+      accept: 'application/vnd.github.v3.text-match+json',
+    },
   });
 
-  console.log(response);
+  const getResponse = await octokit.request('GET /users/{username}', {
+    username: name,
+  });
+
+  console.log('Search Response: ', searchResponse);
+  console.log('-----');
+  console.log('Get Response: ', getResponse);
 }
 
 export default getUser;
