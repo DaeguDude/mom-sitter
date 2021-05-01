1e65b7f0970c8e1094349c6897e9713ca0ee62e7
Sanghak <k3hppk@gmail.com>
2021-05-01 12:23:00 +0900

    Create createUser function that creates user HTML element
    This creates 'createUser' function which will create HTML element
    dynamically.
    
    This just returns document fragment(which is HTML element), not
    appends.
    
    Issues: #10

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,28 @@
 import addHeader from './addHeader';
 import addNav from './addNav';
+import createUser from './createUser';
 import '../styles/style.css';
+
+const userInfo = {
+  login: 'DaeguDude',
+  avatar_url: 'https://avatars.githubusercontent.com/u/35420073?v=4',
+  is_favorite: true,
+};
+
+const userInfo2 = {
+  login: 'DaeguDude',
+  avatar_url: 'https://avatars.githubusercontent.com/u/35420073?v=4',
+  is_favorite: false,
+};
 
 function App() {
   const render = () => {
     addHeader();
     addNav('api');
+
+    const userListSection = document.querySelector('.userlist-section');
+    userListSection.appendChild(createUser(userInfo));
+    userListSection.appendChild(createUser(userInfo2));
   };
 
   return { render };
