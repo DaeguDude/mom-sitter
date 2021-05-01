64f3963ffc7597b9651dafef4fb12853c48d5c0e
Sanghak <k3hppk@gmail.com>
2021-05-01 12:01:45 +0900

    Create createSearchBar function that creates searchBar HTML
    This creates 'createSearchBar' function which will create HTML element
    dynamically.
    
    Something to note is that this just returns document fragment
    (which is HTML element), not appends.
    
    From where to append this has to be thought.
    
    Issues: #10

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import addHeader from './addHeader';
 import createSearchTab from './createSearchTab';
+import createSearchBar from './createSearchBar';
 import '../styles/style.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     addHeader();
     const userListSection = document.querySelector('.userlist-section');
     userListSection.appendChild(createSearchTab('api'));
+    userListSection.appendChild(createSearchBar());
   };
 
   return { render };
