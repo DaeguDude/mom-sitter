aa59543452ddfa0c1739b0128b932607bbbb5fcb
Sanghak <k3hppk@gmail.com>
2021-05-02 11:59:54 +0900

    Add Hot loading for CSS and automate dist directory generation
    This commit enables hot loading with CSS by configuring
    webpack-dev-server hot to true.
    
    And this also automate the dist directory generation, meanwhile
    cleaning the files that are not needed. This would be useful
    for the future when there are multiple bundle files.
    
    Issue: #13

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '깃허브 사용자 검색 앱',
+      template: 'src/index.html',
+    }),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    hot: true,
   },
   module: {
     rules: [
@@ -21,5 +25,10 @@ module.exports = {
   },
   experiments: {
     topLevelAwait: true,
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
