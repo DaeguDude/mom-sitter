36c65b60a8ab9647480d41943dee1d2175fc8c5a
Sanghak <k3hppk@gmail.com>
2021-05-02 23:31:50 +0900

    Add missing comments

--- src/components/addMain.js
+++ src/components/addMain.js
@@ -4,6 +4,9 @@ import createUsersRow from './createUsersRow';
  * 검색된 사용자 리스트를 받아, 화면에 렌더링을 하여줌
  *
  * @param {Object} currentSearchResult - 검색된 사용자 리스트
+ * @param {string} currentSearchResult[].avatar_url - 유저 이미지 주소
+ * @param {string} currentSearchResult[].login - 유저 이름
+ * @param {boolean} currentSearchResult[].is_favorite - 유저 즐겨찾기 등록 여부
  * @param {function} onFavoriteHandler - 즐겨찾기를 추가 삭제 해주는 콜백 함수
  * @returns {}
  */
@@ -31,8 +34,17 @@ if (module.hot) {
   module.hot.accept('./createUser.js', function () {});
 }
 
-function groupByFirstLetter(objectArray) {
-  return objectArray.reduce((acc, obj) => {
+/**
+ * 사용자 리스트를 받아, 사용자 이름의 첫 글자같은 사용자들끼리 그룹으로 묶어 객체를 반환해 줌
+ *
+ * @param {Object} userList - 검색된 사용자 리스트
+ * @param {string} userList[].avatar_url - 유저 이미지 주소
+ * @param {string} userList[].login - 유저 이름
+ * @param {boolean} userList[].is_favorite - 유저 즐겨찾기 등록 여부
+ * @returns
+ */
+function groupByFirstLetter(userList) {
+  return userList.reduce((acc, obj) => {
     // obj.name의 첫번째 글자가 키로 존재하는지 확인
     const firstLetter = obj.login[0].toLowerCase();
     if (!acc[firstLetter]) {
--- src/helpers/getUserList.js
+++ src/helpers/getUserList.js
@@ -28,6 +28,16 @@ async function getUserList(name, favorites) {
   return sortUserByAlphabet(userList);
 }
 
+/**
+ * 깃허브 응답을 받아 프로필 이미지, 유저이름, 즐겨찾기 여부를 담은 사용자 리스트를 반환
+ *
+ * @param {Object} response - 깃허브로 부터 받아온 search 응답
+ * @param {Object[]} favorites - 즐겨찾기에 등록된 사용자 리스트
+ * @param {string} favorites[].avatar_url - 유저 이미지 주소
+ * @param {string} favorites[].login - 유저 이름
+ * @param {boolean} favorites[].is_favorite - 유저 즐겨찾기 등록 여부
+ * @returns {Object[]} - search 응답에서 필요한 것들만 간추려 새로운 사용자 리스트를 돌려 줌
+ */
 function makeNewUserList(response, favorites) {
   const userList = response.data.items;
 
@@ -41,6 +51,16 @@ function makeNewUserList(response, favorites) {
   return newUserList;
 }
 
+/**
+ * 유저 이름을 받아, 즐겨찾기에 등록되어 있는 여부를 확인하여 줌
+ *
+ * @param {string} userName - 유저 이름
+ * @param {Object[]} favorites - 즐겨찾기에 등록된 사용자 리스트
+ * @param {string} favorites[].avatar_url - 유저 이미지 주소
+ * @param {string} favorites[].login - 유저 이름
+ * @param {boolean} favorites[].is_favorite - 유저 즐겨찾기 등록 여부
+ * @returns {boolean} - 즐겨찾기 등록 여부
+ */
 function doesExistInFavorites(userName, favorites) {
   if (favorites === null) {
     return false;
